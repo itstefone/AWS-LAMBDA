c7fa85fb36f5e67f8401d194a0faedd921bb8781
stefone <stefan.aksentijevic.ks@gmail.com>
2021-05-17 13:34:14 +0200

    initial

---
+++ .aws-sam/build/getByCityForecastFunction/src/handlers/get-forecast.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+exports.handler= async (event) => {
+
+    let city = event.pathParameters.city;
+
+    let url =   `https://api.weatherapi.com/v1/current.json?key=aa4af4aba2ac4f229d390048211705&q=${city}&aqi=no
+    `;
+
+    let body = await axios.get(url);
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(body.data)
+    };
+
+
+    return response;
+}
